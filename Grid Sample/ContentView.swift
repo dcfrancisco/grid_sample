6fab518560114d324431c029128013a9d117305f
danny francisco <dcfran@gmail.com>
2024-06-02 16:39:37 +0800

    Initial commit

--- Grid Sample/ContentView.swift
+++ Grid Sample/ContentView.swift
@@ -1,26 +1,32 @@
-//
-//  ContentView.swift
-//  Grid Sample
-//
-//  Created by Danny Francisco on 6/2/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    private var data: [Int] = Array(1...40)
+    private let colors: [Color] = [.red, .blue,.green,.yellow]
+    
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 170))
+    ]
+    //
+    var body: some View{
+        NavigationView(){
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns,spacing: 20){
+                    ForEach(data, id: \.self)  { number in
+                        ZStack{
+                            Rectangle()
+                                .frame(width: 170,  height: 170)
+                                .foregroundColor(colors[number%4])
+                                .cornerRadius(30, antialiased: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                            Text("\(number)")
+                                .foregroundColor(.white)
+                                .font(.system(size: 80, weight: .medium, design: .rounded))
+                            
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Grid Sample")
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
