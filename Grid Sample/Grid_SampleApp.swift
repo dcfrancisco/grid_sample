ad229651b4aac9d6e871f3aaf329813ef6d16707
danny francisco <dcfran@gmail.com>
2024-06-02 16:22:40 +0800

    Initial Commit

---
+++ Grid Sample/Grid_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grid_SampleApp.swift
+//  Grid Sample
+//
+//  Created by Danny Francisco on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Grid_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
